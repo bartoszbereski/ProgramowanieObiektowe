3790e5c454891989ab79e1c6d258f7ced967ec77
Bartosz <bartelousmk@gmail.com>
2022-10-13 20:57:52 +0200

    Lab 1 done

---
+++ Programowanie/src/HelloDate.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class HelloDate {
+	public static void main(String[] args) {
+		Date data = new Date();
+		System.out.print("Hello! ");
+		System.out.println(data);
+		
+	}
+}
