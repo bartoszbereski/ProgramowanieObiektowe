9e64eb8fda7936d12a06d89cb25c7f0640780c28
Bartosz <bartelousmk@gmail.com>
2022-10-14 17:21:28 +0200

    lab 1 done

--- Lab_01/Programowanie/src/HelloDate2.java
+++ Lab_01/Programowanie/src/HelloDate2.java
@@ -1,22 +1,12 @@
 import java.util.Date;
-import java.util.Locale;
-import java.text.DateFormat;
-import java.text.Format;
 import java.text.SimpleDateFormat;
 public class HelloDate2 {
 	public static void main(String[] args) {
+		
 		System.out.print("Witaj! Teraz jest ");
 		String pattern = "yyyy-MM-dd HH:mm";
-		
-		Locale loc = new Locale("en", "US");
-		
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-
-		
-		//DateFormat df = DateFormat.getDateInstance(DateFormat.DEFAULT, loc);
-		
 		String date = simpleDateFormat.format(new Date());
-		
 		System.out.println(date);
 		
 		
