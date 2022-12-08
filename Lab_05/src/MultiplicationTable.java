94bb20879ae8fb7fd313523bec3f342f6aa36ae8
bartoszbereski <101666686+bartoszbereski@users.noreply.github.com>
2022-12-08 12:36:37 +0100

    fixed

--- Lab_05/src/MultiplicationTable.java
+++ Lab_05/src/MultiplicationTable.java
@@ -1,7 +1,4 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,9 +7,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MultiplicationTable {
-	public static void main(String[] args) throws IOException {
-		Properties prop = new Properties();
-	    File file;
+    public static void main(String[] args) throws IOException {
+        Properties prop = new Properties();
+        File file;
         try {
             file = new File("setup.properties");
         } catch (Exception exception) {
@@ -20,34 +17,37 @@ public class MultiplicationTable {
             return;
         }
         if (!file.exists()) {
-    	 prop.setProperty("wartosc_minimum", "1");
-         prop.setProperty("wartosc_maximum", "10");
-         prop.setProperty("powtorzen_minimum", "10");
-         prop.setProperty("powtorzen_maximum", "25");
-         prop.setProperty("procent", "70");
-         prop.store(new FileWriter("setup.properties"), "Multiplication Table settings");
+            prop.setProperty("wartosc_minimum", "1");
+            prop.setProperty("wartosc_maximum", "10");
+            prop.setProperty("powtorzen_minimum", "10");
+            prop.setProperty("powtorzen_maximum", "25");
+            prop.setProperty("procent", "70");
+            prop.store(new FileWriter("setup.properties"), "Multiplication Table settings");
         }
         FileReader reader = new FileReader(file);
         prop.load(reader);
-		Random rand = new Random();
-		int randz = Integer.parseInt((String) prop.get("wartosc_maximum"));
-		
+        Random rand = new Random();
+        int randz = Integer.parseInt((String) prop.get("wartosc_maximum"));
+
         Scanner scanner = new Scanner(System.in);
         int goodans = 0; int questionsCounter = 0; double percent;
         while (true) {
-        	int n = rand.nextInt(randz);
-    		int m = rand.nextInt(randz);
-    		n+=1;m+=1;
-        	System.out.print(m+" * "+n+" = ");
+            int n = rand.nextInt(randz);
+            int m = rand.nextInt(randz);
+            n+=1;m+=1;
+            System.out.print(m+" * "+n+" = ");
             int mul = scanner.nextInt();
             questionsCounter= questionsCounter+1;
-            if((m*n) == mul) { goodans=goodans+1; }
+            if((m*n) == mul) {
+                goodans=goodans+1;
+            }
             percent = 100*goodans/questionsCounter;
-            if(questionsCounter == 25) {break;}
-            if(questionsCounter >= 10 ) {
-            	if(percent >= 70) {
-            		break;}
-            	}
+            if(questionsCounter == Integer.parseInt((String) prop.get("powtorzen_maximum"))){
+                break;
+            }
+            if(questionsCounter >= Integer.parseInt((String) prop.get("powtorzen_minimum"))) {
+                if(percent >= 70) {break;}
+            }
             if(goodans/questionsCounter < 70) {continue;}
         }
         System.out.println("procent poprawnych odpowiedzi "+percent+"%");
