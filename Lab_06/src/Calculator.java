833911485cdea43a7d5b76007111266d2cc19092
Bartosz Bereski <bartelousmk@gmail.com>
2023-01-19 23:46:42 +0100

    all done

--- Lab_06/src/Calculator.java
+++ Lab_06/src/Calculator.java
@@ -4,26 +4,142 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.*;
 import java.awt.event.*;
-import java.text.DecimalFormat;
 
-public class Calculator implements ActionListener {
-    JFrame f;
-    JTextField textfield;
-    JButton[] NumButtons = new JButton[10];
-    JButton[] FuncButton = new JButton[6];
-    JButton addButton, difButton, mulButton, divButton;
-    JButton eqButton, clcButton;
-    JPanel panel;
-    Font scoreFont = new Font("Arial", Font.BOLD, 16);
-    double num1 = 0, num2 = 0, result = 0, tmp = 0;
-    String previous, current, operator;
+public class Calculator {
+    static JFrame f;
+    static JTextField textfield;
+    static JButton[] NumButtons = new JButton[10];
+    static JButton[] FuncButton = new JButton[6];
+    static JButton addButton, difButton, mulButton, divButton;
+    static JButton eqButton, clcButton;
+    static JPanel panel;
+    private static final Font scoreFont = new Font("Arial", Font.BOLD, 16);
+    private static int num1 = 0, num2 = 0, result = 0;
+    static String operator;
+    static boolean dividingByZero, lastInputEqual = false, lastInputAction = true, solved = true, hasBeenSolved = true;
 
-    Calculator() {
-        current = "";
-        previous = "";
-        f = new JFrame("My First Calculator - v.0.1");
+    public static void selectOperator(String newOperator) {
+        if (lastInputAction) {
+            operator = newOperator;
+            solved = false;
+            return;
+        }
+        if (textfield.getText().isEmpty()) {
+            num1 = 0;
+            operator = newOperator;
+            solved = false;
+            lastInputAction = true;
+            lastInputEqual = false;
+            return;
+        }
+        if (!solved) {
+            calculate();
+            solved = true;
+        }
+        operator = newOperator;
+        num1 = Integer.parseInt(textfield.getText());
+        solved = false;
+        lastInputAction = true;
+        lastInputEqual = false;
+    }
+
+    public static void calculate() {
+        if (operator == null) {
+            solved = true;
+            lastInputEqual = false;
+            lastInputAction = true;
+            hasBeenSolved = true;
+            if (!textfield.getText().isEmpty()) {
+                num1 = Integer.parseInt(textfield.getText());
+            }
+            return;
+        }
+        if (lastInputEqual) {
+            num1 = result;
+        } else {
+            result = num1;
+            num2 = Integer.parseInt(textfield.getText());
+        }
+        switch (operator) {
+            case "*":
+                result = num1 * num2;
+                break;
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    clcButton.doClick();
+                    textfield.setForeground(Color.red);
+                    textfield.setText("ERROR: DIVISION BY ZERO");
+                    dividingByZero = true;
+                    return;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                break;
+        }
+        textfield.setText(String.valueOf(result));
+        lastInputEqual = true;
+        lastInputAction = false;
+        solved = true;
+    }
+
+    static ActionListener myActionListener = new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            for (int i = 0; i < 10; i++) {
+                if (e.getSource().equals(NumButtons[i])) {
+                    textfield.setForeground(Color.black);
+                    if (lastInputEqual) {
+                        textfield.setText("");
+                        operator = null;
+                    }
+                    if (lastInputAction) {
+                        textfield.setText("");
+                    }
+                    textfield.setText(textfield.getText().concat(NumButtons[i].getText()));
+                    lastInputAction = false;
+                    lastInputEqual = false;
+                }
+            }
+            if (e.getSource().equals(addButton)) {
+                selectOperator(addButton.getText());
+            }
+            if (e.getSource().equals(difButton)) {
+                selectOperator(difButton.getText());
+            }
+            if (e.getSource().equals(mulButton)) {
+                selectOperator(mulButton.getText());
+            }
+            if (e.getSource().equals(divButton)) {
+                selectOperator(divButton.getText());
+            }
+            if (e.getSource().equals(eqButton)) {
+                calculate();
+            }
+            if (e.getSource().equals(clcButton)) {
+                textfield.setText("");
+                num1 = 0;
+                num2 = 0;
+                operator = null;
+                lastInputEqual = false;
+                lastInputAction = true;
+                hasBeenSolved = true;
+                dividingByZero = false;
+                textfield.setForeground(Color.black);
+            }
+        }
+    };
+
+    public static void createAndShowGUI() {
+        f = new JFrame("My First Calculator - v.3.1");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(600, 250);
+        f.setSize(400, 250);
         f.setLocationRelativeTo(null);
         f.setResizable(false);
         f.setLayout(null);
@@ -49,16 +165,15 @@ public class Calculator implements ActionListener {
         FuncButton[5] = clcButton;
 
         for (int i = 0; i < 6; i++) {
-            FuncButton[i].addActionListener(this);
+            FuncButton[i].addActionListener(myActionListener);
             FuncButton[i].setFocusable(false);
         }
         for (int i = 0; i < 10; i++) {
             NumButtons[i] = new JButton(String.valueOf(i));
-            NumButtons[i].addActionListener(this);
+            NumButtons[i].addActionListener(myActionListener);
             NumButtons[i].setFocusable(false);
         }
-
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setBounds(0, 60, 400, 200);
         panel.setLayout(new GridLayout(5, 4, 10, 5));
         panel.add(NumButtons[1]);
@@ -78,100 +193,21 @@ public class Calculator implements ActionListener {
         panel.add(FuncButton[4]);
         panel.add(FuncButton[3]);
 
+        f.setBackground(Color.black);
+        panel.setBackground(Color.lightGray);
+        f.getContentPane().setBackground(Color.lightGray);
+
         f.add(panel);
         f.add(textfield);
         f.setVisible(true);
 
     }
 
-    public void selectOperator(String newOperator) {
-        // if no number is entered yet
-        if (current.isEmpty()) {
-            operator = newOperator;
-            return;
-        }
-        if (!previous.isEmpty()) {
-            calculate();
-        }
-        operator = newOperator;
-        previous = current;
-        current = "";
-    }
-
-    public void updateOutput() {
-        textfield.setText(current);
-    }
-
-    public void calculate() {
-        if (previous.length() < 1 || current.length() < 1) {
-            return;
-        }
-        double result = 0.0;
-        double num1 = Double.parseDouble(previous);
-        double num2 = Double.parseDouble(current);
-        switch (operator) {
-            case "*":
-                result = num1 * num2;
-                break;
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "÷":
-                result = num1 / num2;
-                break;
-            default:
-                break;
-        }
-        current = String.valueOf(result);
-        operator = null;
-        previous = "";
-    }
-
-    public void appendToOutput(String num) {
-        current += num;
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        for (int i = 0; i < 10; i++) {
-            if (e.getSource() == NumButtons[i]) {
-                appendToOutput(NumButtons[i].getText());
-            }
-        }
-        if (e.getSource() == addButton) {
-            num1 = Double.parseDouble(textfield.getText());
-            selectOperator(addButton.getText());
-        }
-        if (e.getSource() == difButton) {
-            num1 = Double.parseDouble(textfield.getText());
-            selectOperator(difButton.getText());
-        }
-
-        if (e.getSource() == mulButton) {
-            num1 = Double.parseDouble(textfield.getText());
-            selectOperator(mulButton.getText());
-        }
-
-        if (e.getSource() == divButton) {
-            num1 = Double.parseDouble(textfield.getText());
-            selectOperator(divButton.getText());
-        }
-        if (e.getSource() == clcButton) {
-            current = "";
-            previous = "";
-            operator = null;
-        }
-        updateOutput();
-        if (e.getSource() == eqButton) {
-            calculate();
-        }
-        updateOutput();
-    }
-
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
